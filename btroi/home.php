a2b1186927b9d69b8f321ae6a0ee90b2e343fbcd
Benjamin Fenner <fenfam@sc.rr.com>
2020-07-01 15:35:55 -0400

    btroi_003 | Initial commit of styles and home page, missing header/footter.

---
+++ btroi/home.php
@@ -0,0 +1,19 @@
+<?php
+
+$title = 'BoomTown - Technical Assessment - Home';
+
+include('header.php');
+
+echo '
+  <div class="style_me">Choose your adventure:</div>
+  <div class="style_me">
+    <a href="btroi/output_data.php">Output Data</a>
+  </div>
+    <div class="style_me">
+    <a href="btroi/perform_verifications.php">Perform Verifications</a>
+  </div>
+  <br>
+  <br>
+  <br>';
+
+include('footer.php');
